2c9f29c8583883892aa68294723cf9e6d2e89c9b
AKHIL KUMAR TS <ak2072719@gmail.com>
2024-12-12 16:50:48 +0530

    login page

---
+++ groceryshop/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .models import *
+import os
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+
+def gro_login(req):
+
+        return render(req,'login.html')
